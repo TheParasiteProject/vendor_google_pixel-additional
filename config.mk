3a29de8a6d9cdb76709c8f5de163f084c287797a
Arseniy Graur <argraur@aosip.dev>
2020-02-11 10:34:41 +0000

    hentai: Ship CarrierSettings, replacing apns-conf and CarrierConfig
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>

--- config.mk
+++ config.mk
@@ -1,2 +1,5 @@
 # Inherit from apex config
 $(call inherit-product, vendor/pixel-additional/configs/apex.mk)
+
+# Inherit from telephony config
+$(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
