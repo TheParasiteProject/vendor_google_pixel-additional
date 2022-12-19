c8c37ee161f3288fef82ecdf4ef7d9702c016a26
someone5678 <nemui3353@gmail.com>
2022-12-20 00:17:32 +0900

    pixel-additional: gapps: Initial gapps config

--- config.mk
+++ config.mk
@@ -1,2 +1,5 @@
 # Inherit from telephony config
 $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
+
+# Inherit from gapps config
+$(call inherit-product, vendor/pixel-additional/gapps/config.mk)
