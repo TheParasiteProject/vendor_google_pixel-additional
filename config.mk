e53175604ca669cffc68543d459f21a4053ab554
someone5678 <nemui3353@gmail.com>
2022-12-20 01:45:53 +0900

    pixel-additional: config: Include Custom Theme Icons if exists
    * Only if INCLUDE_PIXEL_LAUNCHER is set to true
    
    Corresponding Repo:
    https://github.com/PixelExperience/packages_overlays_ThemeIcons

--- config.mk
+++ config.mk
@@ -3,3 +3,8 @@ $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
 
 # Inherit from gapps config
 $(call inherit-product, vendor/pixel-additional/gapps/config.mk)
+
+ifeq ($(INCLUDE_PIXEL_LAUNCHER),true)
+# Include Custom Theme Icons for Pixel Launcher if exists
+$(call inherit-product-if-exists, packages/overlays/ThemeIcons/config.mk)
+endif
