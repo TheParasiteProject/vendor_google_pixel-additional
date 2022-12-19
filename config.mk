33058fe0063c1a459e2b8c3d063a0ea424c23dee
someone5678 <nemui3353@gmail.com>
2022-12-20 02:02:30 +0900

    pixel-additional: overlay: Override default wallpaper
    * From Pixel 7 Pro

--- config.mk
+++ config.mk
@@ -1,6 +1,9 @@
 # Inherit from telephony config
 $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
 
+# Inherit from overlay config
+$(call inherit-product, vendor/pixel-additional/configs/overlay.mk)
+
 # Inherit from gapps config
 $(call inherit-product, vendor/pixel-additional/gapps/config.mk)
 
