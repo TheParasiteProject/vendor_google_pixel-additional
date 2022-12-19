74e418dfe1fec9c9adc747cd299b2155fce9626e
someone5678 <nemui3353@gmail.com>
2022-12-20 02:48:30 +0900

    pixel-additional: gapps: ExcludePackages: Exclude PixelSetupWizardStringsOverlay
    * Use PixelSetupWizard's default strings instead

--- gapps/ExcludePackages/Android.mk
+++ gapps/ExcludePackages/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ExcludePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES += \
+	PixelSetupWizardStringsOverlay
 ifeq ($(TARGET_INCLUDE_PIXEL_THEMES),true)
 LOCAL_OVERRIDES_PACKAGES += \
 	StatixThemePicker \
