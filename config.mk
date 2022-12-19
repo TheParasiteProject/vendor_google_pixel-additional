65cc514d33d24a4445e3a5e80f82755900854f8c
someone5678 <nemui3353@gmail.com>
2022-12-20 00:13:47 +0900

    pixel-additional: Drop apex/gapps
    * StatiXOS has own implementation of Updatable Apex

--- config.mk
+++ config.mk
@@ -1,5 +1,2 @@
-# Inherit from apex config
-$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
-
 # Inherit from telephony config
 $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
