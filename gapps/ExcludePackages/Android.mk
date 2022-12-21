1950c4fd405ace2dc55c241502106edc60fe3971
someone5678 <nemui3353@gmail.com>
2022-12-21 16:23:13 +0900

    pixel-additional: gapps: ExcludePackages: Exclude apex related duplicated packages
    * Those are already included in partner_modules with different name

--- gapps/ExcludePackages/Android.mk
+++ gapps/ExcludePackages/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ExcludePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-
+LOCAL_OVERRIDES_PACKAGES += \
+	PrebuiltGoogleTelemetryTvp \
+	PrebuiltGoogleAdservicesTvp
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
