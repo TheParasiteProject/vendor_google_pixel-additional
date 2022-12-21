4cd725f6462cc5412189d124dd8101af4664ff3a
someone5678 <nemui3353@gmail.com>
2022-12-21 16:50:48 +0900

    pixel-additional: apex: Restore and Clean-up overlays
    * Remove duplicates with partner_modules

--- configs/apex.mk
+++ configs/apex.mk
@@ -1,6 +1,30 @@
 # Anything including updatable_apex.mk should have done so by now.
 ifeq ($(TARGET_FLATTEN_APEX), false)
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
+
+ifneq ($(filter NowPlayingOverlay, $(modules-get-list)),)
+PRODUCT_PACKAGES += \
+	ApexNowPlayingOverlay
+else
+PRODUCT_PACKAGES += \
+	ApexOverlay
+endif
+
+# Overlay
+PRODUCT_PACKAGES += \
+	ApexSettingsOverlay \
+	NetworkStackOverlay \
+	MediaProviderOverlay \
+	PixelConfigOverlayCommonExtra \
+	PixelDocumentsUIGoogleOverlayExtra \
+	PixelTetheringOverlay \
+	CaptivePortalLoginOverlayExtra \
+	CellBroadcastReceiverOverlay \
+	CellBroadcastServiceOverlayExtra \
+	GoogleConfigBluetoothOverlay \
+	GoogleConfigOverlayExtra \
+	GooglePermissionControllerOverlayExtra
+
 else
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_flatten_apex.mk)
 endif
