8d9a77bcdb24289eec3be08e4b78452e62d5ae7a
someone5678 <nemui3353@gmail.com>
2022-12-21 16:05:21 +0900

    pixel-additional: Inherit StatiXOS' partner modules
    * partner_modules support both updatable apex and flattened apex

--- config.mk
+++ config.mk
@@ -1,5 +1,8 @@
 # Inherit from telephony config
 $(call inherit-product, vendor/pixel-additional/configs/telephony.mk)
 
+# Inherit from apex config
+$(call inherit-product, vendor/pixel-additional/configs/apex.mk)
+
 # Inherit from gapps config
 $(call inherit-product, vendor/pixel-additional/gapps/config.mk)
---
+++ configs/apex.mk
@@ -0,0 +1,6 @@
+# Anything including updatable_apex.mk should have done so by now.
+ifeq ($(TARGET_FLATTEN_APEX), false)
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
+else
+$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_flatten_apex.mk)
+endif
