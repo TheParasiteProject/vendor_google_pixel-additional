1daa458d4517b746b9dabb3e2a9cf5d520850495
someone5678 <nemui3353@gmail.com>
2023-01-15 04:34:49 +0900

    pixel-additional: apex: Conditionally enable additional wifi/uwb modules

--- configs/apex.mk
+++ configs/apex.mk
@@ -1,5 +1,15 @@
 # Anything including updatable_apex.mk should have done so by now.
 ifeq ($(TARGET_FLATTEN_APEX), false)
+
+
+ifneq ($(MAINLINE_INCLUDE_UWB_MODULE), false)
+MAINLINE_INCLUDE_UWB_MODULE := true
+endif
+
+ifneq ($(MAINLINE_INCLUDE_WIFI_MODULE), false)
+MAINLINE_INCLUDE_WIFI_MODULE := true
+endif
+
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
 
 ifneq ($(filter NowPlayingOverlay, $(modules-get-list)),)
