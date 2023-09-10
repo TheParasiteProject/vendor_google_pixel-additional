fad462e7c68c7c097fede1bec4a5e00a32ebe4e2
someone5678 <nemui3353@gmail.com>
2023-09-10 10:30:20 +0900

    pixel-additional: Check DONT_COPY_BT_CERT before copying BT cert
    * Some devices can't use Google's prebuilt Bluetooth APEX
    
    * For those device that using AOSP Bluetooth APEX,
      don't copy BT certs to prevent sepolicy breakages
    
    Change-Id: If78aeb5ad79c3c6c91679a187c451d319eb7a600

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,5 +13,8 @@ repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune 
 
 rm -Rf vendor/aosp/rro_overlays/GooglePermissionControllerOverlay
 
-rm -Rf vendor/partner_modules/build/certificates/bluetooth.x509.pem
-cp vendor/pixel-additional/products/apex/certificates/bluetooth.x509.pem vendor/partner_modules/build/certificates/
+if [ ! "$DONT_COPY_BT_CERT" = true ];
+then
+    rm -Rf vendor/partner_modules/build/certificates/bluetooth.x509.pem
+    cp vendor/pixel-additional/products/apex/certificates/bluetooth.x509.pem vendor/partner_modules/build/certificates/
+fi
