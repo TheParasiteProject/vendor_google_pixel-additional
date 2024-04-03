fb7a92800afac0d39a3e6b8c985fc605f35a2aee
someone5678 <someone5678@users.noreply.github.com>
2024-04-03 17:28:08 +0900

    pixel-additional: Move out mainline_modules inclusion guard
    Change-Id: I7a56c4afa71b05bfd8d34599809ebd834de07576

--- config.mk
+++ config.mk
@@ -1,10 +1,8 @@
 # Inherit from telephony config
 $(call inherit-product, vendor/google/pixel-additional/products/telephony.mk)
 
-ifneq ($(TARGET_SHIP_GMS),false)
 # Inherit from apex config
 $(call inherit-product, vendor/google/mainline_modules/config.mk)
-endif
 
 # Inherit from gms config
 $(call inherit-product, vendor/google/pixel-additional/products/gms.mk)
