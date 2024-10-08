efbb8102eb134472015397e811e4119cd5b80641
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-09 01:26:21 +0900

    pixel-additional: Add missing blobs
    Change-Id: Icfd7b1a63e4047fd4b7df45a4ad081d01e37d15d

--- products/telephony.mk
+++ products/telephony.mk
@@ -32,11 +32,11 @@ PRODUCT_COPY_FILES += \
     vendor/google/pixel-additional/radio/proprietary/product/etc/permissions/com.android.sdm.plugins.diagmon.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.sdm.plugins.diagmon.xml \
     vendor/google/pixel-additional/radio/proprietary/product/etc/sysconfig/allowlist_com.shannon.imsservice.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.shannon.imsservice.xml \
     vendor/google/pixel-additional/radio/proprietary/system_ext/etc/permissions/com.shannon.imsservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.shannon.imsservice.xml \
-    vendor/google/pixel-additional/radio/proprietary/system_ext/etc/permissions/com.shannon.rcsservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.shannon.rcsservice.xml \
-    vendor/google/pixel-additional/radio/proprietary/system_ext/lib64/libimsmedia.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libimsmedia.so \
-    vendor/google/pixel-additional/radio/proprietary/system_ext/lib64/libmediaadaptor.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libmediaadaptor.so
+    vendor/google/pixel-additional/radio/proprietary/system_ext/etc/permissions/com.shannon.rcsservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.shannon.rcsservice.xml
 
 PRODUCT_PACKAGES += \
+    libpixelimsmedia \
+    libmediaadaptor \
     DiagMon \
     ImsMediaService \
     ShannonIms \
