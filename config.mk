d542b6fb145ac95603cf45bf269be2cfd08893cd
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-11-25 05:14:04 +0900

    pixel-additional: Gutted out all of stuffs
    Change-Id: I9e36e0917ec6a348209eb7aa3f90832090881bec

--- config.mk
+++
@@ -1,5 +0,0 @@
-# Inherit from telephony config
-$(call inherit-product, vendor/google/pixel-additional/products/telephony.mk)
-
-# Inherit from gms config
-$(call inherit-product, vendor/google/pixel-additional/products/gms.mk)
